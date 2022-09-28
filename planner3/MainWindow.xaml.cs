d03a1b0cfbdf75d4ddc173b5ec2c467e412a4999
Vika <wmaksimova@bk.ru>
2022-09-28 21:03:08 +0300

    Всё сломали

--- planner3/MainWindow.xaml.cs
+++ planner3/MainWindow.xaml.cs
@@ -26,11 +26,7 @@ namespace planner3
             date_text.Text = DateTime.Today.ToLongDateString();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            calendar_planner.Visibility = Visibility;
-            date_text.Visibility = Visibility.Hidden;
-        }
+        
 
         private void calendar_planner_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
         {
@@ -38,6 +34,42 @@ namespace planner3
             date_text.Text = data.ToLongDateString();
             calendar_planner.Visibility = Visibility.Hidden;
             date_text.Visibility = Visibility;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            int n = int.Parse(count_of_affairs.Text);
+            if (n == 7)
+            {
+                MessageBox.Show("Нельзя записать больше 7 дел на день");
+
+            }
+            else
+            {
+                n++;
+                count_of_affairs.Text = n.ToString();
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            int n = int.Parse(count_of_affairs.Text);
+            if (n == 1)
+            {
+                MessageBox.Show("Ты чего дурак какие 0 дел иди работай");
+            }
+            else
+            {
+                n--;
+                count_of_affairs.Text = n.ToString();
+            }
+        }
+
+
+        private void button_calendar_Click(object sender, RoutedEventArgs e)
+        {
+            calendar_planner.Visibility = Visibility;
+            date_text.Visibility = Visibility.Hidden;
         }
     }
 }
